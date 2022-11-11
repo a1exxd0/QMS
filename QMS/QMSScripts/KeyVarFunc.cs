4da0da3cbcf3c0a8f2673d4f529be065e5ac12c0
Alex Do <22122@mynsb.co.uk>
2022-11-11 21:27:25 +0000

    test commit

---
+++ QMS/QMSScripts/KeyVarFunc.cs
@@ -0,0 +1,17 @@
+﻿namespace QSim.Scripts
+{
+    //Newtonsoft.Json
+    //System.Data.SQL
+    //System.Net.Sockets
+    public class KeyVarFunc
+    {
+        private static string? username;
+
+        public static string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+    }
+}
+ 
