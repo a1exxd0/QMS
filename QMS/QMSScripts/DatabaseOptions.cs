f1eaab4f4bdbd46a92a6efe55b1fddf063f8b605
Alex Do <22122@mynsb.co.uk>
2022-11-11 22:32:23 +0000

    test 2

--- QMS/QMSScripts/DatabaseOptions.cs
+++ QMS/QMSScripts/DatabaseOptions.cs
@@ -9,7 +9,7 @@ namespace QMS.QMSScripts
     public class DatabaseOptions
     {
         #region Constants
-        public static string dataSource = "AlexLddaptop";
+        public static string dataSource = "AlexLaptop";
         public static string initialCatalog = "LoginAndAccessInfo";
         #endregion
     }
