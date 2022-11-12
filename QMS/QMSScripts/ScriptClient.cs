a9f284043f163c2acc1d88545bc731d317bd1133
Alex Do <22122@mynsb.co.uk>
2022-11-12 18:45:05 +0000

    commenting added

--- QMS/QMSScripts/ScriptClient.cs
+++ QMS/QMSScripts/ScriptClient.cs
@@ -3,19 +3,30 @@ using System.Net.Sockets;
 using System.Text;
 
 namespace QMS.QMSScripts;
+/// <summary>
+/// Set of functions to send data to other side through specific ports
+/// </summary>
 public class ClientFunctions
 {
+    //declarations for IP/Socket combinations for every class transferred
     public static readonly IPEndPoint ciSocket = new(DatabaseOptions.ServerIP, 31050);
+
+    /// <summary>
+    /// Sends string through ConnectionInitialization-specific port
+    /// </summary>
+    /// <param name="s">string to be sent</param>
     public static async void SendCI(string s)
     {
+        //open up a socket object to correct destination
         using Socket client = new(ciSocket.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        //execute while loop as soon as connection is established
         await client.ConnectAsync(ciSocket);
-        s = s + "<|EOM|>";
+        s = s + "<|EOM|>"; //add endofmessage indicator
         while (true)
         {
             var messageBytes = Encoding.UTF8.GetBytes(s);
-            _ = await client.SendAsync(messageBytes, SocketFlags.None);
-            Console.WriteLine("sent ci");
+            _ = await client.SendAsync(messageBytes, SocketFlags.None); 
+            Console.WriteLine("sent ci");//execute once message has sent
 
             var buffer = new byte[1024];
             var recieved = await client.ReceiveAsync(buffer, SocketFlags.None);
@@ -24,7 +35,7 @@ public class ClientFunctions
             {
                 Console.WriteLine("acknowledged");
                 break;
-            }
+            } //file is fully correctly recieved.
         }
 
         client.Shutdown(SocketShutdown.Both);
--- QMS/QMSScripts/ScriptServer.cs
+++ QMS/QMSScripts/ScriptServer.cs
@@ -1,24 +1,30 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace QMS.QMSScripts;
+/// <summary>
+/// Set of functions to recieve data and identify what port it is in
+/// </summary>
 public class ServerFunctions
 {
+    //declarations for IP/Socket combinations for every class transferred
     public static readonly IPEndPoint ciReciever = new(IPAddress.Any, 31050);
-    public static async Task<string> RecieveCI()
+    public static async Task<ConnectionInitializer> RecieveCI()
     {
         using Socket listener = new(SocketType.Stream, ProtocolType.Tcp);
-        listener.Bind(ciReciever);
+        listener.Bind(ciReciever);//accepts connections from all ip addresses
+                        //listening from port 31050
         listener.Listen(100);
-        var handler = await listener.AcceptAsync();
+        var handler = await listener.AcceptAsync(); //accept connection
         while (true)
         {
             var buffer = new byte[1024];
             var recieved = await handler.ReceiveAsync(buffer, SocketFlags.None);
             var response = Encoding.UTF8.GetString(buffer, 0, recieved);
             var eom = "<|EOM|>";
-            if (response.IndexOf(eom) > -1 /* end of message*/)
+            if (response.IndexOf(eom) > -1 /* end of message detected*/)
             {
                 Console.WriteLine("message recieved");
 
@@ -26,8 +32,10 @@ public class ServerFunctions
                 var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
                 await handler.SendAsync(echoBytes, 0);
                 Console.WriteLine("acknowledgement sent");
-
-                return response.Replace(eom, "");
+                //convert to object and return
+                ConnectionInitializer? ci = JsonConvert.DeserializeObject<ConnectionInitializer>
+                    (response.Replace(eom, "")); 
+                return ci;
             }
         }
     }
