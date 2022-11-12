0470478d5d1c843e27bd3a47b7a22159ac8d030e
Alex Do <alexxdo2004@gmail.com>
2022-11-12 17:59:20 +0000

    Updated tested client

--- QMS/QMSScripts/ScriptServer.cs
+++ QMS/QMSScripts/ScriptServer.cs
@@ -1,25 +1,26 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace QMS.QMSScripts;
 public class ServerFunctions
 {
+    public static readonly IPEndPoint ciReciever = new(IPAddress.Any, 31050);
     public static async Task<string> RecieveCI()
     {
         using Socket listener = new(SocketType.Stream, ProtocolType.Tcp);
-        listener.Listen(31050);
+        listener.Bind(ciReciever);
+        listener.Listen(100);
         var handler = await listener.AcceptAsync();
         while (true)
         {
             var buffer = new byte[1024];
             var recieved = await handler.ReceiveAsync(buffer, SocketFlags.None);
             var response = Encoding.UTF8.GetString(buffer, 0, recieved);
-
             var eom = "<|EOM|>";
             if (response.IndexOf(eom) > -1 /* end of message*/)
             {
                 Console.WriteLine("message recieved");
-                Console.WriteLine(response.Replace(eom, ""));
 
                 var ackMessage = "<|ACK|>";
                 var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
