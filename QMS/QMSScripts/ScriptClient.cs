8f9bc662504dd6066bb7d6d7dba2d76c8702526c
Alex Do <22122@mynsb.co.uk>
2022-11-12 16:54:58 +0000

    ConnectionInitializer ports and sending

--- QMS/QMSScripts/ScriptClient.cs
+++ QMS/QMSScripts/ScriptClient.cs
@@ -1,8 +1,31 @@
-﻿using Newtonsoft.Json;
+﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace QMS.QMSScripts;
-public class ScriptClient
+public class ClientFunctions
 {
-    public 
+    public readonly IPEndPoint ciSocket = new(DatabaseOptions.ServerIP, 31050);
+    public async void SendCI(string s)
+    {
+        using Socket client = new(ciSocket.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        await client.ConnectAsync(ciSocket);
+        while (true)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(s);
+            _ = await client.SendAsync(messageBytes,SocketFlags.None);
+            Console.WriteLine("sent ci");
+
+            var buffer = new byte[1024];
+            var recieved = await client.ReceiveAsync(buffer, SocketFlags.None);
+            var response = Encoding.UTF8.GetString(buffer, 0, recieved);
+            if (response == "<|ACK|>")
+            {
+                Console.WriteLine("acknowledged");
+                break;
+            }
+        }
+
+        client.Shutdown(SocketShutdown.Both);
+    }
 }
--- QMS/QMSScripts/ScriptServer.cs
+++ QMS/QMSScripts/ScriptServer.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace QMS.QMSScripts;
-public class ScriptServer
+public class ServerFunctions
 {
+    public async Task<string> RecieveCI()
+    {
+        using Socket listener = new(SocketType.Stream, ProtocolType.Tcp);
+        listener.Listen(31050);
+        var handler = await listener.AcceptAsync();
+        while (true)
+        {
+            var buffer = new byte[1024];
+            var recieved = await handler.ReceiveAsync(buffer, SocketFlags.None);
+            var response = Encoding.UTF8.GetString(buffer, 0, recieved);
 
+            var eom = "<|EOM|>";
+            if (response.IndexOf(eom) > -1 /* end of message*/)
+            {
+                Console.WriteLine("message recieved");
+                Console.WriteLine(response.Replace(eom, ""));
+
+                var ackMessage = "<|ACK|>";
+                var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+                await handler.SendAsync(echoBytes, 0);
+                Console.WriteLine("acknowledgement sent");
+
+                return response.Replace(eom, "");
+            }
+        }
+    }
 }
