ff6ddc2b3a9e471c998c3676702d94fb5dfc1536
Alex Do <22122@mynsb.co.uk>
2023-03-12 10:43:54 +0000

    New files made

---
+++ QMS/QMSScripts/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using QMS.Views;
+
+
+namespace QMS.Networking;
+internal class MessageController
+{
+}
